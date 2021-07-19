bcde66239c3dbd96a95715552a14d7da3f1c8744
root <root@lucifer.demon>
2021-07-19 12:35:11 +0530

    non-functional test-UI

--- sliderUI.py
+++ sliderUI.py
@@ -1,8 +1,2 @@
-from tkinter import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-master = Tk()
-for i in range(50):
-	w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
-	w.pack()
-
-mainloop()
